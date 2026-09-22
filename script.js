51e09b39a9194dbd4ffb6942c31a6002b918ca3a
Manu-Gurgel <manuelavmgurgel@gmail.com>
2026-09-22 17:42:07 -0300

    window

--- script.js
+++ script.js
@@ -17,3 +17,60 @@ function openWindow(windowId){
 function closeWindow(windowId){
    document.getElementById(windowId).style.display="none";
 }
+
+function Windowmovel(windowElement){
+    let estamovendo = false;
+    let offsetX = 0;
+    let offsetY = 0;
+
+
+    
+   function começarmovimento(e, clientX, clientY) {
+      if(!e.target.classList.contains('drag-zone')) return;
+
+      estamovendo=true;
+      offsetX=e.clientX-windowElement.offsetLeft;
+      offsetY=e.clientY-windowElement.offsetTop;
+      windowElement.style.zIndex = "1000";
+
+      if(e.cancetable) e.preventDefault();
+    };
+
+    function movendo(clientX, clientY){
+       if(!estamovendo) return;
+
+       let newX = e.clientX - offsetX;
+       let newY = e.clientY-offsetY;
+
+       if (newX < 0) newX = 0;
+       if (newY < 0) newY = 0;
+
+       windowElement.style.left = newX + 'px';
+       windowElement.style.top = newY + 'px';
+    };
+
+    function pararmovimento() {
+      estamovendo=false;
+    };
+
+
+    // ouvintes para evento do mouse //
+    windowElement.addEventListener('mousedown', (e) => começarmovimento(e, e.clientX, e.clientY));
+    document.addEventListener('mousemove', (e)=> movendo(e.clientX, e.clientY));
+    document.addEventListenet('mouseup', (e) => pararmovimento);
+
+    windowElement.addEventListener('touchstart', (e) => {
+      const touch = e.touches[0]; // considera só o primeiro dedo que toca
+      começarmovimento(e, touch.clientX, touch.clientY);
+    }, {passive: false}); 
+
+    document.addEventListener('touchmove', (e) =>{
+      const touch = e.touches[0];
+      movendo(touch.clientX, touch.clientY);
+    }, {passive: false});
+
+    document.addEventListener('touchend', pararmoviemnto);
+}
+
+const windowselect = document.getElementById(windowId);
+Windowmovel(windowselect);
